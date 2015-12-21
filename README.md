b163d0d99b66bb13b4d70b2d81ea809ae8b588f4
Matt Sheehan <sheehamj@outlook.com>
2015-12-21 00:14:13 -0500

    font updates, build warnings fixed

---
+++ README.md
@@ -0,0 +1,35 @@
+## Building mattsheehan.me ##
+
+#### NodeJS ####
+
+###### Install nodejs ######
+```
+$> sudo apt-get install nodejs
+```
+
+###### Install npm ######
+```
+$> sudo apt-get install npm
+```
+
+###### Install LESS ######
+```
+$> sudo npm install -g less
+```
+Note: the path to `lessc` cmd should be `/usr/local/bin/lessc` <br/>
+Yout might have to specify `nodejs <lessc path> <input files>`
+
+#### Ruby ####
+
+###### Install Ruby ######
+Refer to [ruby installation](https://github.com/sheehamj13/mint-17-3-help/blob/master/ruby/ruby-installation.md)
+
+###### Install Jekyll ######
+```
+$> gem install jekyll
+```
+
+###### Install jekyll-paginate ######
+```
+$> gem install jekyll-paginate
+```
--- _plugins/Jekyll-Rss-Feed.rb
+++ _plugins/Jekyll-Rss-Feed.rb
@@ -4,7 +4,7 @@
 #        attributes in the _config.yml file
 #
 # Uses the following attributes in _config.yml:
-#   rss_title 	 	- (optional) the feed title 
+#   rss_title 	 	- (optional) the feed title
 #   rss_author 	 	- (optional) the feed author
 #   rss_description - (optional) short description of the site
 #   rss_link		- (optional) url of the site
@@ -28,7 +28,7 @@ Jekyll::Hooks.register :site, :post_write do |site|
 	author = site.config["rss_author"] || ""
 	description = site.config["rss_description"] || ""
 	link = site.config["rss_link"] || ""
-	date = site.posts.map { |post| post.date }.max
+	date = site.posts.docs.map { |post| post.date }.max
 
 	# write rss
 	rss = RSS::Maker.make("2.0") do |rss|
@@ -39,9 +39,9 @@ Jekyll::Hooks.register :site, :post_write do |site|
 		rss.channel.updated = date
 		rss.channel.copyright = date.year
 
-		count = [site.posts.count, 20].min
+		count = [site.posts.docs.count, 20].min
 
-		site.posts.reverse[0..count].each do |post|
+		site.posts.docs.reverse[0..count].each do |post|
 			# post.render(site.layouts, site.site_payload)
 			rss.items.new_item do |item|
 				item.title = post.data[:title]
@@ -57,77 +57,3 @@ Jekyll::Hooks.register :site, :post_write do |site|
 	filepath = File.join(feed_dir, 'rss.xml')
 	File.open(filepath, "w") { |f| f.write(rss) }
 end
-
-
-# module Jekyll
-
-# 	# Generator
-# 	# Creates rss feed pages
-# 	class Rss < Generator
-# 		priority :low
-# 		safe true
-
-
-# 		def generate(site)
-# 			# create feed page and add to static files
-# 			page = Feed.new(site, site.dest, "/feeds/", "rss.xml")
-# 			site.static_files << page
-# 		end
-
-# 	end
-
-
-# 	# Rss Feed Page
-# 	class Feed < StaticFile
-# 		def initialize(site, base, dir, name)
-# 			super(site, base, dir, name, nil)
-# 		end
-
-# 		def write(dest)
-# 			require "rss"
-
-# 			# create directory if doesn't exist
-# 			dirpath = File.join(dest, @dir)
-# 			FileUtils.mkdir_p(dirpath)
-
-
-# 			# configs
-# 			title = @site.config["rss_title"] || ""
-# 			author = @site.config["rss_author"] || ""
-# 			description = @site.config["rss_description"] || ""
-# 			link = @site.config["rss_link"] || ""
-# 			date = @site.posts.map { |post| post.date }.max
-
-# 			# write rss
-# 			rss = RSS::Maker.make("2.0") do |rss|
-# 				rss.channel.title = title
-# 				rss.channel.link = link
-# 				rss.channel.description = description
-# 				rss.channel.author = author
-# 				rss.channel.updated = date
-# 				rss.channel.copyright = date.year
-
-# 				count = [@site.posts.count, 20].min
-
-# 				@site.posts.reverse[0..count].each do |post|
-# 					# post.render(@site.layouts, @site.site_payload)
-# 					rss.items.new_item do |item|
-# 						item.title = post.data[:title] #.title
-# 						item.link = "#{link}#{post.url}"
-# 						item.description = post.content
-# 						item.updated = post.date
-# 						item.guid.content = "#{link}#{post.url}"
-# 					end
-# 				end
-# 			end
-
-# 			# write file
-# 			filepath = File.join(dirpath, @name)
-# 			puts filepath
-# 			File.open(filepath, "w") { |f| f.write(rss) }
-# 		end
-
-# 	end
-
-
-# end
