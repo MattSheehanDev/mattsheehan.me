0ac361f76ebdd54d378a61da950e28f930b090d6
Matt Sheehan <sheehamj@outlook.com>
2019-01-02 22:39:14 -0500

    added autoprefixer, alot of cleaning up left to do

--- Gemfile
+++ Gemfile
@@ -7,5 +7,8 @@ group :jekyll_plugins do
     gem "jekyll-paginate", "1.1.0"
     gem "jekyll-feed", "0.11.0"
     gem "jekyll-admin", "0.8.1"
+    gem "autoprefixer-rails", "9.4.3"
+    gem "jekyll-assets", "3.0.12"
 	gem "builder", "3.2.2"
 end
+
--- _plugins/jekyll_less.rb
+++ _plugins/jekyll_less.rb
@@ -7,6 +7,7 @@
 # Notes
 # => css files will have the same root name as their less counterparts
 
+require 'autoprefixer-rails'
 
 Jekyll::Hooks.register :site, :post_write do |site|
   statics = site.static_files
@@ -20,17 +21,74 @@ Jekyll::Hooks.register :site, :post_write do |site|
 
   statics.delete_if do |file|
     next if not File.extname(file.path) == '.less'
-
     less_name = File.basename(file.path)
 
     css_dir = File.join(site.dest, css_dest)
     css_name = less_name.gsub(/\.less$/i, ".css")
-    css_path = File.join(css_dir, css_name)
+    output_path = File.join(css_dir, css_name)
 
     FileUtils.mkdir_p(css_dir);
-    system("#{lessc} #{file.path} #{css_path}")
+
+    css = `#{lessc} #{file.path}`
+    # css = system("#{lessc} #{file.path}")
+    # system("#{lessc} #{file.path} #{output_path}")
+
+    # # add autoprefixer
+    # css = File.read(css_path)
+    result = AutoprefixerRails.process(css,
+        from: output_path,
+        to:   output_path,
+        browsers: ['cover 99.5%']
+        # map:  { prev: File.read(map), inline: false }
+    )
+    File.open(output_path, 'w') { |io| io << result.css }
 
     true
   end
 
 end
+
+
+
+# Jekyll::Hooks.register :site, :post_write do |site|
+#     statics = site.static_files
+#     config = site.config
+  
+#     # lessc = config['lessc'] || 'lessc'
+#     # css_dest = config['css_dest'] || '/'
+  
+#     # raise "Missing 'lessc' path in site configuration" if not lessc
+  
+  
+#     statics.delete_if do |file|
+#       next if not File.extname(file.path) == '.less'
+  
+#       less_name = File.basename(file.path)
+  
+#       css_dir = File.join(site.dest, css_dest)
+#       css_name = less_name.gsub(/\.less$/i, ".css")
+#       css_path = File.join(css_dir, css_name)
+  
+#       FileUtils.mkdir_p(css_dir);
+#       system("#{lessc} #{file.path} #{css_path}")
+  
+#       true
+#     end
+  
+#   end
+
+
+# on_stylesheet_saved do |file|
+#     css = File.read(file)
+#     map = file + '.map'
+  
+#     if File.exists? map
+#       result = AutoprefixerRails.process(css,
+#         from: file,
+#         to:   file,
+#         map:  { prev: File.read(map), inline: false })
+#       File.open(file, 'w') { |io| io << result.css }
+#       File.open(map,  'w') { |io| io << result.map }
+#     else
+#       File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
+#     end
